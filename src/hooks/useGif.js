e014f1ba6467b59eb79fdd810f5717c891785b7d
guptagarima2301 <guptagarima2301@gmail.com>
2024-08-19 13:11:13 +0530

    first commit

---
+++ src/hooks/useGif.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+const API_KEY = "w9EQ7XWsSu7ZNCaEQ9DfOqqi2ujfP8Ni";
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+
+const useGif = (tag) => {
+    const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState('false');
+
+  
+    async function fetchData(tag) {
+      setLoading(true);
+      
+      const {data} = await axios.get(tag ? `${url}&tag=${tag}`  : url);
+      const imageSource = data.data.images.downsized_large.url;
+      setGif(imageSource);
+      setLoading(false);
+    }
+    
+    useEffect( () => {
+      fetchData('car');
+    },[] )
+
+    return {gif,loading, fetchData};
+}
+
+export default useGif
